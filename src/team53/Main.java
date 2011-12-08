17a4b71b315df3fffd577f68e6794f1a35b1de59
Michael Stevens <mls0.5mv2@gmail.com>
2011-12-07 19:03:05 -0500

    formatting

--- src/team53/Main.java
+++ src/team53/Main.java
@@ -2,19 +2,16 @@ package team53;
 
 import lejos.nxt.Button;
 
-
 public class Main {
 	public static void main(String[] args) {
-	    Robot robot = new Robot();
-	    robot.calibrate(44, 12f, 0.05f, 0.5f);
-	    
-	    Button.ENTER.waitForPressAndRelease();
-		
-	    while(!Button.ENTER.isPressed())
-	    	robot.forward(10);
-		  
-	    
-	    
-	  }
+		Robot robot = new Robot();
+		robot.calibrate(44, 12f, 0.05f, 0.5f);
+
+		Button.ENTER.waitForPressAndRelease();
+
+		while (!Button.ENTER.isPressed())
+			robot.forward(10);
+
+	}
 
 }
--- src/team53/Robot.java
+++ src/team53/Robot.java
@@ -6,82 +6,60 @@ import lejos.util.PIDController;
 /**
  * Robot that stops if it hits something before it completes its travel.
  */
-public class Robot {	
-	LightSensor sensor;	
+public class Robot {
+	LightSensor sensor;
 	PIDController pid;
-	int speed = 600;	
+	int speed = 600;
 
 	public Robot() {
-		sensor = new LightSensor(SensorPort.S1);	 
+		sensor = new LightSensor(SensorPort.S1);
 	}
-	
+
 	public void calibrate(int target, int kp, int ki, int kd) {
 		pid = new PIDController(target);
 		pid.setPIDParam(PIDController.PID_KP, kp);
 		pid.setPIDParam(PIDController.PID_KI, ki);
-		pid.setPIDParam(PIDController.PID_KD, kd);		
+		pid.setPIDParam(PIDController.PID_KD, kd);
 	}
-	
+
 	public void calibrate(int target, float kp, float ki, float kd) {
 		pid = new PIDController(target);
 		pid.setPIDParam(PIDController.PID_KP, kp);
 		pid.setPIDParam(PIDController.PID_KI, ki);
-		pid.setPIDParam(PIDController.PID_KD, kd);		
+		pid.setPIDParam(PIDController.PID_KD, kd);
 	}
-	/*
-	 * 	private team53.Color getColor() {
 
-		team53.Color c = null;
-		int result = 0;
-		for(int i = 0;i<50; i++) {
-			result += sensor.getColorID();
-		}
-		result /= 50;
-		System.out.println(result);
-		
-		switch (sensor.getColorID()) {
-		case 6:
-			c = team53.Color.White;
-			break;
-		case 3:
-			c = team53.Color.Yellow;
-			break;
-		case 5:
-			c = team53.Color.Red;
-			break;
-		case 0:
-			c = team53.Color.Red;
-			break;
-		case 2:
-			c = team53.Color.Blue;
-			break;
-		case 8:
-			c = team53.Color.Black;
-			break;
-		case 1:
-			c = team53.Color.Green;
-			break;
-		
-		}
-		
-		return c;
+	/*
+	 * private team53.Color getColor() {
+	 * 
+	 * team53.Color c = null; int result = 0; for(int i = 0;i<50; i++) { result
+	 * += sensor.getColorID(); } result /= 50; System.out.println(result);
+	 * 
+	 * switch (sensor.getColorID()) { case 6: c = team53.Color.White; break;
+	 * case 3: c = team53.Color.Yellow; break; case 5: c = team53.Color.Red;
+	 * break; case 0: c = team53.Color.Red; break; case 2: c =
+	 * team53.Color.Blue; break; case 8: c = team53.Color.Black; break; case 1:
+	 * c = team53.Color.Green; break;
+	 * 
+	 * }
+	 * 
+	 * return c; }
+	 */
+
+	public void turnLeft() {
+
 	}
-	*/
-	
-	public void turnLeft() {	
-		
+
+	public void turnRight() {
+
 	}
-	
-	public void turnRight() {	
-		
-	}	
-	
-	public void forward(double num) {		
-		int value = pid.doPID(sensor.getLightValue());		
-		
+
+	public void forward(double num) {
+		int value = pid.doPID(sensor.getLightValue());
+
 		Motor.B.setSpeed(speed + value);
 		Motor.B.forward();
 		Motor.C.setSpeed(speed - value);
-		Motor.C.forward();		
+		Motor.C.forward();
 	}
 }
