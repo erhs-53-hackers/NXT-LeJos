6f92c63abe403cfd761e2352204abdff29c5e7db
Michael Stevens <mls0.5mv2@gmail.com>
2011-12-07 19:01:36 -0500

    Cleaning up code

--- src/team53/Main.java
+++ src/team53/Main.java
@@ -1,29 +1,18 @@
 package team53;
 
 import lejos.nxt.Button;
-import lejos.nxt.Motor;
-import lejos.robotics.navigation.DifferentialPilot;
+
 
 public class Main {
 	public static void main(String[] args) {
-	    Robot robot = new Robot(44);
-	    robot.pilot = new DifferentialPilot(2.4f, 6.8f, Motor.B, Motor.C);
-	    //robot.pilot.rotate(90);
+	    Robot robot = new Robot();
+	    robot.calibrate(44, 12f, 0.05f, 0.5f);
+	    
 	    Button.ENTER.waitForPressAndRelease();
-		while(!Button.ENTER.isPressed())
-		{
-	    robot.forward(10);
-		}
-	    /*
-	    robot.forward(30);
-	    robot.turnRight();
-	    robot.forward(30);
-	    robot.turnRight();
-	    robot.forward(30);
-	    robot.turnRight();
-	    robot.forward(30);
-	    robot.turnRight();
-	    */
+		
+	    while(!Button.ENTER.isPressed())
+	    	robot.forward(10);
+		  
 	    
 	    
 	  }
